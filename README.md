e8ed18c6123106023d95a94ba11305844a136517
Brendon <425026+brendon-codes@users.noreply.github.com>
2013-10-27 11:05:02 -0700

    Cleanups and documentation fixes

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 Python Bosh Client
 ==================
 
+Usage
+-----
+
+    from bosh_client import BOSHClient
+    
+    username = "foo"
+    password = "bar"
+    bosh_url = "http://example.com/bosh"
+    client = BOSHClient(username, password, bosh_url)
+
+    if client.logged_in:
+        print("Success!")
+    else:
+        print("Fail!")
+    
--- __init__.py
+++ __init__.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python
+
 """
-Bosh Client Helpers
+Bosh Client for Python
+
+Author: Brendon Crawford
+Author: Nathan Fritz
+Author: Jack Moffit
 """
 
 import sys
 import os
 import httplib
-import urllib
 import uuid
 import random
 import base64
@@ -13,7 +18,6 @@ from urlparse import urlparse
 import logging
 from xml.dom import minidom
 
-from django.conf import settings
 
 logger = logging.getLogger('bosh_client')
 
@@ -25,12 +29,12 @@ SESSION_XMLNS = 'urn:ietf:params:xml:ns:xmpp-session'
 XMPP_XMLNS = 'urn:xmpp:xbosh'
 
 
-class JID:
+class JID(object):
     """
     JID Class
 
-    @note This was taken from SleekXMPP
-    @see sleekxmpp.basexmpp.set_jid
+    Note: This was taken from SleekXMPP
+    See: sleekxmpp.basexmpp.set_jid
     """
 
     fulljid = None
@@ -43,49 +47,51 @@ class JID:
         """
         Rip a JID apart and claim it as our own.
 
-        @author BrendonCrawford
-        @author NathanFritz
-        @param jid String
-        @constructor
+        Args:
+            jid (string)
         """
         self.fulljid = jid
         self.resource = self.getjidresource(jid)
         self.jid = self.getjidbare(jid)
         self.user = jid.split('@', 1)[0]
         self.host = jid.split('@', 1)[-1].split('/', 1)[0]
+        return
 
     def getjidresource(self, fulljid):
         """
         Jid Resource
 
-        @author BrendonCrawford
-        @author NathanFritz
-        @param fulljid String
-        @return List
+        Args:
+            fulljid (String)
+
+        Returns:
+            String
         """
         if '/' in fulljid:
             return fulljid.split('/', 1)[-1]
-        else:
-            return ''
+        return ''
 
     def getjidbare(self, fulljid):
         """
         Bare Jid
 
-        @author BrendonCrawford
-        @author NathanFritz
-        @param fulljid String
-        @return List
+        Args:
+            fulljid (String)
+
+        Returns:
+            String
         """
         return fulljid.split('/', 1)[0]
 
 
-class BOSHClient:
+class BOSHClient(object):
     """
     BOSHClient Class
 
-    @see punjab.httpb_client
+    Note: Inspired by punjab
+    See: punjab.httpb_client
     """
+    # pylint: disable=R0902,R0914,R0915
 
     rid = None
     jid = None
@@ -97,14 +103,16 @@ class BOSHClient:
     headers = None
     bosh_service = None
 
-    def __init__(self, jabberid, password, bosh_service, hold='1', wait='60'):
+    def __init__(self, jabberid, password, bosh_service, hold=1, wait=60):
         """
-        @constructor
-        @author BrendonCrawford
-        @author JackMoffit
-        @param jabberid
-        @param password
-        @param bosh_service
+        Constructor
+
+        Args:
+            jabberid (String)
+            password (String)
+            bosh_service (String)
+            hold (Int)
+            wait (Int)
         """
         logger.debug("BOSHClient jabberid:%s; password:%s; bosh_service:%s",
                      jabberid, password, bosh_service)
@@ -119,18 +127,21 @@ class BOSHClient:
             "Accept": "text/xml"
         }
         self.bosh_service = urlparse(bosh_service)
-        self.logged_in = self.startSessionAndAuth(hold, wait)
+        self.logged_in = self.start_session_and_auth(hold, wait)
         logger.debug("BOSH Logged In: %s", self.logged_in)
+        return
 
-    def buildElement(self, name, child=None, attrs=None):
+    def build_element(self, name, child=None, attrs=None):
         """
         Builds xml element
 
-        @author BrendonCrawford
-        @param name
-        @param child
-        @param attrs
-        @return minidom.Element
+        Args:
+            name (String)
+            child
+            attrs
+
+        Returns:
+            minidom.Element
         """
         if attrs is None:
             attrs = {}
@@ -141,27 +152,30 @@ class BOSHClient:
             e.appendChild(child)
         return e
 
-    def buildText(self, data):
+    def build_text(self, data):
         """
         Builds xml text element
 
-        @author BrendonCrawford
-        @param data
-        @return minidom.Text
+        Args:
+            data
+
+        Returns:
+            minidom.Text
         """
         t = minidom.Text()
         t.data = data
         return t
 
-    def buildBody(self, child=None, attrs=None):
+    def build_body(self, child=None, attrs=None):
         """
         Build a BOSH body.
 
-        @author BrendonCrawford
-        @author JackMoffit
-        @param child
-        @param attrs
-        @return minidom.Element
+        Args:
+            child
+            attrs
+
+        Returns:
+            minidom.Element
         """
         if attrs is None:
             attrs = {}
@@ -171,26 +185,24 @@ class BOSHClient:
         body['content'] = 'text/xml; charset=utf-8'
         body['rid'] = str(self.rid)
         body['xml:lang'] = 'en'
-        if self.sid != None:
+        if self.sid is not None:
             body['sid'] = str(self.sid)
-        for attr_k, attr_v in attrs.items():
+        for attr_k, attr_v in attrs.iteritems():
             body[attr_k] = attr_v
-        elm = self.buildElement('body', child=child, attrs=body)
+        elm = self.build_element('body', child=child, attrs=body)
         return elm
 
-    def sendBody(self, body):
+    def send_body(self, body):
         """
         Send the body.
 
-        @author BrendonCrawford
-        @author JackMoffit
-        @param body
-        @return Tuple(minidom.Element, String)
+        Args:
+            body (String)
+
+        Returns:
+            Tuple[minidom.Element, String]
         """
-        # start new session
         out = body.toxml()
-        #print "######"
-        #print out
         conn = httplib.HTTPConnection(self.bosh_service.netloc)
         conn.request("POST", self.bosh_service.path, out, self.headers)
         response = conn.getresponse()
@@ -198,23 +210,20 @@ class BOSHClient:
         if response.status == 200:
             data = response.read()
         conn.close()
-        #print "------"
-        #print data
         doc = minidom.parseString(data)
         return (doc.documentElement, data)
 
-    def buildAuthStringPlain(self):
+    def build_auth_string_plain(self):
         """
         Builds auth string
 
-        @author BrendonCrawford
-        @author JackMoffit
-        @return String
+        Returns:
+            String
         """
-        auth_str = ""
-        auth_str += "\000"
+        auth_str = ur""
+        auth_str += ur"\u0000"
         auth_str += self.jabberid.user.encode('UTF-8')
-        auth_str += "\000"
+        auth_str += ur"\u0000"
         try:
             auth_str += self.password.encode('UTF-8').strip()
         except UnicodeDecodeError:
@@ -222,38 +231,39 @@ class BOSHClient:
         enc_str = base64.b64encode(auth_str)
         return enc_str
 
-    def uniqueId(self):
+    def unique_id(self):
         """
         Returns a Unique ID
 
-        @author BrendonCrawford
-        @return String
+        Returns:
+            String
         """
         ret = uuid.uuid4().hex
         return ret
 
-    def startSessionAndAuth(self, hold='1', wait='2'):
+    def start_session_and_auth(self, hold=1, wait=2):
         """
         Starts session and authenticates
 
-        @author BrendonCrawford
-        @author JackMoffit
-        @param hold
-        @param wait
-        @return Bool
+        Args:
+            hold (Int)
+            wait (Int)
+
+        Returns:
+            Bool
         """
         # Create a session
         # create body
         body = {}
-        body['hold'] = hold
+        body['hold'] = str(hold)
         body['to'] = self.jabberid.host
-        body['wait'] = wait
+        body['wait'] = str(wait)
         body['window'] = '5'
         body['ver'] = '1.6'
         body['xmpp:version'] = '1.0'
         body['xmlns:xmpp'] = XMPP_XMLNS
-        body_elm = self.buildBody(attrs=body)
-        retb, _data = self.sendBody(body_elm)
+        body_elm = self.build_body(attrs=body)
+        retb, _ = self.send_body(body_elm)
         if retb.hasAttribute('authid') and retb.hasAttribute('sid'):
             self.authid = retb.getAttribute('authid')
             self.sid = retb.getAttribute('sid')
@@ -261,38 +271,39 @@ class BOSHClient:
             auth['xmlns'] = SASL_XMLNS
             auth['mechanism'] = 'PLAIN'
             if auth['mechanism'] == 'PLAIN':
-                auth_str = self.buildAuthStringPlain()
-                auth_text_elm = self.buildText(auth_str)
-                auth_elm = self.buildElement('auth', child=auth_text_elm,
-                                             attrs=auth)
-                body_auth = self.buildBody(child=auth_elm)
-                retb, _data = self.sendBody(body_auth)
+                auth_str = self.build_auth_string_plain()
+                auth_text_elm = self.build_text(auth_str)
+                auth_elm = self.build_element('auth',
+                                              child=auth_text_elm,
+                                              attrs=auth)
+                body_auth = self.build_body(child=auth_elm)
+                retb, _ = self.send_body(body_auth)
                 sucs = retb.getElementsByTagName('success')
                 if len(sucs) > 0:
                     body_binder = {}
                     body_binder['xmpp:restart'] = 'true'
                     body_binder['xmlns:xmpp'] = XMPP_XMLNS
-                    body_binder_elm = self.buildBody(attrs=body_binder)
-                    retb, _data = self.sendBody(body_binder_elm)
+                    body_binder_elm = self.build_body(attrs=body_binder)
+                    retb, _ = self.send_body(body_binder_elm)
                     bind_elms = retb.getElementsByTagName('bind')
                     ## Bind element was found
                     if len(bind_elms) > 0:
                         bind = {}
                         bind['xmlns'] = BIND_XMLNS
-                        bind_elm = self.buildElement('bind', attrs=bind)
+                        bind_elm = self.build_element('bind', attrs=bind)
                         if self.jabberid.resource:
                             resource_text = \
-                                self.buildText(self.jabberid.resource)
-                            resource = self.buildElement('resource',
-                                                         child=resource_text)
+                                self.build_text(self.jabberid.resource)
+                            resource = self.build_element('resource',
+                                                          child=resource_text)
                             bind_elm.appendChild(resource)
                         iq = {}
                         iq['xmlns'] = 'jabber:client'
                         iq['type'] = 'set'
-                        iq['id'] = self.uniqueId()
-                        iq_elm = self.buildElement('iq', bind_elm, iq)
-                        iq_body = self.buildBody(child=iq_elm)
-                        retb, _data = self.sendBody(iq_body)
+                        iq['id'] = self.unique_id()
+                        iq_elm = self.build_element('iq', bind_elm, iq)
+                        iq_body = self.build_body(child=iq_elm)
+                        retb, _ = self.send_body(iq_body)
                         jids = retb.getElementsByTagName('jid')
                         ## Jid was found
                         if len(jids) > 0:
@@ -301,32 +312,34 @@ class BOSHClient:
                             iq = {}
                             iq['xmlns'] = 'jabber:client'
                             iq['type'] = 'set'
-                            iq['id'] = self.uniqueId()
+                            iq['id'] = self.unique_id()
                             session = {}
                             session['xmlns'] = SESSION_XMLNS
                             sess_elm = \
-                                self.buildElement('session', attrs=session)
+                                self.build_element('session', attrs=session)
                             sess_iq_elm = \
-                                self.buildElement('iq', sess_elm, attrs=iq)
+                                self.build_element('iq', sess_elm, attrs=iq)
                             sess_body_elm = \
-                                self.buildBody(sess_iq_elm)
-                            retb, _data = self.sendBody(sess_body_elm)
+                                self.build_body(sess_iq_elm)
+                            retb, _ = self.send_body(sess_body_elm)
                             sess_res = retb.getElementsByTagName('session')
                             ## Session returned ok
                             if len(sess_res) > 0:
                                 self.rid += 1
                                 return True
         return False
+    # pylint: enable=R0902,R0914,R0915
 
 
 if __name__ == '__main__':
+    root_logger = logging.getLogger()
+    root_logger.addHandler(logging.StreamHandler())
     USERNAME = sys.argv[1]
     PASSWORD = sys.argv[2]
     SERVICE = sys.argv[3]
     c = BOSHClient(USERNAME, PASSWORD, SERVICE)
-    print
-    print "Logged In: %s" % c.logged_in
-    print "SID: %s" % c.sid
-    print "JID: %s" % c.jid
-    print "RID: %s" % c.rid
-    print
+    root_logger.info("Logged In: %s", c.logged_in)
+    root_logger.info("SID: %s", c.sid)
+    root_logger.info("JID: %s", c.jid)
+    root_logger.info("RID: %s", c.rid)
+    sys.exit(0)
